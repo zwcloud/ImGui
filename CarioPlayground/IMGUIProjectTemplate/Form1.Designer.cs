6e389fe0ce72d4fc9fe3bb56d5539008df75cf29
zwcloud <zwcloud@hotmail.com>
2015-09-02 14:05:07 +0000

    Add a template project.

---
+++ CarioPlayground/IMGUIProjectTemplate/Form1.Designer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using IMGUI;
+
+namespace IMGUIProjectTemplate
+{
+    partial class Form1
+    {
+        protected override void OnGUI(GUI gui)
+        {
+        }
+    }
+
+}
---
+++ CarioPlayground/IMGUIProjectTemplate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using IMGUI;
+
+namespace IMGUIProjectTemplate
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+    }
+}
