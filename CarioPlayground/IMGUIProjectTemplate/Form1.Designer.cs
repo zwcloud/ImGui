8cac6d2619d915ae715c5c7b9b1c2a4a6a9d6ba4
zwcloud <zwcloud@hotmail.com>
2016-07-08 15:25:43 +0000

    Fix projects' reference to Cairo.

--- CarioPlayground/IMGUIProjectTemplate/Form1.Designer.cs
+++ CarioPlayground/IMGUIProjectTemplate/Form1.Designer.cs
@@ -10,7 +10,14 @@ namespace ImGuiProjectTemplate
     {
         protected override void OnGUI(GUI gui)
         {
-            gui.Button(new Rect(0, 36, 300, 30), "Hello World!", "Button0");
+            var w = this.Size.Width;
+            var h = this.Size.Height;
+            gui.BeginV();
+            {
+                gui.TitleBar(new Rect(0, 0, w, h*0.08), null, "ImGui Project 1", "ApplicationTitle");
+                gui.Button(new Rect(0, 36, 300, 30), "Hello World!", "Button0");
+            }
+            gui.EndV();
         }
     }
 
