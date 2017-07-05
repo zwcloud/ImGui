a3b6ea1112ccec33bd19b408f171d3f0473fa81c
zwcloud <zwcloud@hotmail.com>
2017-07-05 21:29:53 +0800

    Add a HostEditor demo(not completed).

---
+++ demos/HostEditor/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using ImGui;
+using System.Collections.Generic;
+using System;
+
+namespace HostEditor
+{
+    public class MainForm : Form
+    {
+        public MainForm() : base(new Rect(60, 60, 500, 600))
+        {
+        }
+
+        bool open = true;
+
+        List<string> fileLines = new List<string>();
+
+        protected override void OnGUI()
+        {
+            GUILayout.Begin("HostEditor", ref open);
+            if(GUILayout.Button("Load host file"))
+            {
+                LoadHost();
+            }
+            for (int i = 0; i < fileLines.Count; i++)
+            {
+                fileLines[i] = GUILayout.Textbox("hostline"+i, new Size(400, 30), fileLines[i]);
+            }
+            GUILayout.End();
+        }
+
+        private void LoadHost()
+        {
+            fileLines.Clear();
+            var hostFileContent = System.IO.File.ReadAllLines(@"C:\Windows\System32\drivers\etc\hosts");
+            fileLines.AddRange(hostFileContent);
+        }
+    }
+}
---
+++ demos/HostEditor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ImGui;
+
+namespace HostEditor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Init();
+            Application.Run(new MainForm());
+        }
+    }
+}
