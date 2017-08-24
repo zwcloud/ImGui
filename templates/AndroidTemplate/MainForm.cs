13d7b80176f53801aca1be31ac0df02a636b8d15
zwcloud <zwcloud@hotmail.com>
2017-08-24 21:19:46 +0800

    Update Android template project.

--- templates/AndroidTemplate/MainForm.cs
+++ templates/AndroidTemplate/MainForm.cs
@@ -1,4 +1,3 @@
-using System;
 using ImGui;
 using ImGui.Common.Primitive;
 
@@ -6,18 +5,13 @@ namespace AndroidTemplate
 {
     class MainForm : Form
     {
-        //TestUI testUI = new TestUI();
-
         public MainForm(Point position, Size size) : base(new Rect(position, size)) { }
 
-        private bool open = true;
-        private string text = "123XD";
+        Demo demo = new Demo();
 
         protected override void OnGUI()
         {
-            GUI.Begin("Demo", ref open, new Point(60, 60), new Size(400, 400), 1, WindowFlags.Default);
-            text = GUILayout.Textbox("MyTextBox", new Size(400, 600), text);
-            GUI.End();
+            demo.OnGUI();
         }
     }
 }
