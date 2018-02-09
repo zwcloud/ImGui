83009b63981e33d29c4c5541766b33182bb771b8
Zou Wei <zwcloud@hotmail.com>
2018-02-09 14:01:47 +0800

    preparing

---
+++ src/ImGui/Rendering/Node.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImGui.Rendering
+{
+    internal class Node
+    {
+    }
+}
---
+++ src/ImGui/Rendering/RenderTree.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+
+namespace ImGui
+{
+    internal class RenderTree
+    {
+    }
+}
