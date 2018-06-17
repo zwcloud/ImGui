c7025564b4f0db11b1cfea3c79f3b221f1e586de
zwcloud <zwcloud@hotmail.com>
2018-06-17 11:58:50 +0800

    rewrite the test for GUI.Window

--- test/ImGui.UnitTest/Control/WindowFacts.cs
+++ test/ImGui.UnitTest/Control/WindowFacts.cs
@@ -1,39 +1,25 @@
-﻿using System;
-using ImGui.Common.Primitive;
-using Xunit;
+﻿using Xunit;
 
 namespace ImGui.UnitTest
 {
     public class WindowFacts
     {
-        public class Form1 : Form
+        public class TheRunMethod
         {
-            private Action onGUI;
-
-            public Form1(Action onGUI) : base(new Point(400, 300), new Size(800, 600)) { this.onGUI = onGUI; Form.current = this; }
-
-            protected override void OnGUI()
-            {
-                this.onGUI?.Invoke();
-            }
-        }
-        
-
-        public class General
-        {
-            public General()
-            {
-                Application.InitSysDependencies();
-            }
-
             [Fact]
             public void TheWindowShouldBeDrawn()
             {
-                //bool open = true;
-                Application.Run(new Form1(() => {
-                    //GUI.Begin("test window", ref open);
-                    //GUI.End();
-                }));
+                Application.Init();
+
+                var form = new MainForm();
+                bool open = true;
+                form.OnGUIAction = () =>
+                {
+                    GUI.Begin("test window", ref open);
+                    GUI.End();
+                };
+
+                Application.Run(form);
             }
         }
     }
--- test/ImGui.UnitTest/MainForm.cs
+++ test/ImGui.UnitTest/MainForm.cs
@@ -1,15 +1,27 @@
-﻿using ImGui.Common.Primitive;
+﻿using System;
+using ImGui.Common.Primitive;
 
 namespace ImGui.UnitTest
 {
     public class MainForm : Form
     {
+        public Action OnGUIAction { get; set; }
+
+        public MainForm(int width, int height) : base(new Rect(320, 180, width, height))
+        {
+        }
+
+        public MainForm(Rect rect) : base(rect)
+        {
+        }
+
         public MainForm() : base(new Rect(320, 180, 300, 400))
         {
         }
 
         protected override void OnGUI()
         {
+            OnGUIAction?.Invoke();
         }
     }
 }
