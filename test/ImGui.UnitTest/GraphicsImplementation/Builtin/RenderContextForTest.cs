503878bcb23d6dc7479d44416cb15f096685e1eb
Zou Wei <zwcloud@hotmail.com>
2018-10-24 20:41:56 +0800

    move RenderContextForTest out of BuiltinPrimitiveRendererFacts

--- test/ImGui.UnitTest/GraphicsImplementation/Builtin/RenderContextForTest.cs
+++ test/ImGui.UnitTest/GraphicsImplementation/Builtin/RenderContextForTest.cs
@@ -1,57 +1,54 @@
-﻿using System;
-using ImGui.Common.Primitive;
+﻿using ImGui.Common.Primitive;
 using ImGui.OSImplentation.Windows;
+using System;
 
-namespace ImGui.UnitTest.Rendering
+namespace ImGui.UnitTest
 {
-    public partial class BuiltinPrimitiveRendererFacts
+    internal class RenderContextForTest : IDisposable
     {
-        internal class RenderContextForTest : IDisposable
+        //TODO de-cuple with Windows platform
+
+        public Win32Window Window { get; private set; }
+        public Win32OpenGLRenderer Renderer { get; private set; }
+
+        public RenderContextForTest(Size size)
         {
-            //TODO de-cuple with Windows platform
+            Application.Init();
 
-            public Win32Window Window { get; private set; }
-            public Win32OpenGLRenderer Renderer { get; private set; }
+            this.Window = new Win32Window();
+            this.Window.Init(Point.Zero, size, WindowTypes.Regular);
 
-            public RenderContextForTest(Size size)
-            {
-                Application.Init();
+            this.Renderer = new Win32OpenGLRenderer();
+            this.Renderer.Init(this.Window.Pointer, this.Window.ClientSize);
+        }
 
-                this.Window = new Win32Window();
-                this.Window.Init(Point.Zero, size, WindowTypes.Regular);
+        public void Clear()
+        {
+            this.Renderer.Clear(Color.FrameBg);
+        }
 
-                this.Renderer = new Win32OpenGLRenderer();
-                this.Renderer.Init(this.Window.Pointer, this.Window.ClientSize);
-            }
+        public void DrawShapeMesh(Mesh shapeMesh)
+        {
+            Win32OpenGLRenderer.DrawMesh(this.Renderer.shapeMaterial, shapeMesh,
+                (int)this.Window.ClientSize.Width, (int)this.Window.ClientSize.Height);
+        }
 
-            public void Clear()
-            {
-                this.Renderer.Clear(Color.FrameBg);
-            }
+        public void DrawImageMesh(Mesh imageMesh)
+        {
+            Win32OpenGLRenderer.DrawMesh(this.Renderer.imageMaterial, imageMesh,
+                (int)this.Window.ClientSize.Width, (int)this.Window.ClientSize.Height);
+        }
 
-            public void DrawShapeMesh(Mesh shapeMesh)
-            {
-                Win32OpenGLRenderer.DrawMesh(this.Renderer.shapeMaterial, shapeMesh,
-                    (int)this.Window.ClientSize.Width, (int)this.Window.ClientSize.Height);
-            }
+        public void DrawTextMesh(TextMesh textMesh)
+        {
+            Win32OpenGLRenderer.DrawTextMesh(this.Renderer.glyphMaterial, textMesh,
+                (int)this.Window.ClientSize.Width, (int)this.Window.ClientSize.Height);
+        }
 
-            public void DrawImageMesh(Mesh imageMesh)
-            {
-                Win32OpenGLRenderer.DrawMesh(this.Renderer.imageMaterial, imageMesh,
-                    (int)this.Window.ClientSize.Width, (int)this.Window.ClientSize.Height);
-            }
-
-            public void DrawTextMesh(TextMesh textMesh)
-            {
-                Win32OpenGLRenderer.DrawTextMesh(this.Renderer.glyphMaterial, textMesh,
-                    (int)this.Window.ClientSize.Width, (int)this.Window.ClientSize.Height);
-            }
-
-            public void Dispose()
-            {
-                this.Renderer.ShutDown();
-                this.Window.Close();
-            }
+        public void Dispose()
+        {
+            this.Renderer.ShutDown();
+            this.Window.Close();
         }
     }
 }
