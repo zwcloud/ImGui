c523791ba2844963d46d50e0a62b4e01232d507b
Zou Wei <zwcloud@hotmail.com>
2018-11-19 00:12:07 +0800

    fix

--- templates/WebTemplate/build.ps1
+++ templates/WebTemplate/build.ps1
@@ -1,4 +1,4 @@
-﻿# copy essential files from mono_wasm_libs
+﻿# copy essential files from MonoWasm
 Copy-Item -Force -Recurse -Container -Path ./MonoWasm/** -Destination ./ -Exclude @("ReadMe.md", ".git")
 # build the WebTemplate.dll
 csc /target:library /unsafe -out:./managed/WebTemplate.dll /r:./managed/System.Net.Http.dll /r:./managed/WebAssembly.Bindings.dll /r:./managed/WebAssembly.Net.Http.dll Program.cs
