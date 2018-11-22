0ed3c9b360a7b3d933dfea3137bfca666c242992
Zou Wei <zwcloud@hotmail.com>
2018-11-22 12:41:34 +0800

    New way of hosting mono-wasm website.

--- templates/WebTemplate/WebTemplateHost/mono-config.js
+++
@@ -1,7 +0,0 @@
-﻿config = {
- 	vfs_prefix: "managed",
- 	deploy_prefix: "managed",
- 	enable_debugging: 0,
- 	file_list: [ "WebTemplateApp.dll","mscorlib.dll","System.Net.Http.dll","System.dll","Mono.Security.dll","System.Xml.dll","System.Core.dll","WebAssembly.Bindings.dll","WebAssembly.Net.Http.dll" ],
-	add_bindings: function() { Module.mono_bindings_init ("[WebAssembly.Bindings]WebAssembly.Runtime"); }
-}
