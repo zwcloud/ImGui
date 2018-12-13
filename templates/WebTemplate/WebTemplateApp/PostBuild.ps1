696faef7c9c53ad30510477f48f31a9f7977ef90
Zou Wei <zwcloud@hotmail.com>
2018-12-13 11:42:31 +0800

    fixed: cannot find referenced WebAssembly.Bindings.dll; updated mono-wasm so we can use "--search-path" for packager.exe

--- templates/WebTemplate/WebTemplateApp/PostBuild.ps1
+++ templates/WebTemplate/WebTemplateApp/PostBuild.ps1
@@ -1,4 +1,4 @@
 ﻿param([string]$ProjectDir, [string]$TargetPath);
 # publish the website to wwwroot
 cd $ProjectDir
-../MonoWasm/packager.exe `--copy ifnewer `--out .\wwwroot `--asset index.html "$TargetPath"
+../MonoWasm/packager.exe `--copy ifnewer `--out .\wwwroot `--asset index.html "$TargetPath" --search-path ../MonoWasm/framework
