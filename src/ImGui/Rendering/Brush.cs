e24588194b20aa7ad5366e3d83f41b55d7004536
Zou Wei <zwcloud@hotmail.com>
2019-04-04 22:24:34 +0800

    restored Brush and Pen for painting and stroking path primitives

---
+++ src/ImGui/Rendering/Brush.cs
@@ -0,0 +1,12 @@
+﻿using ImGui.Common.Primitive;
+
+namespace ImGui.Rendering
+{
+    /// <summary>
+    /// Defines how to paint an area.
+    /// </summary>
+    internal class Brush
+    {
+        public Color FillColor { get; set; } = Color.Black;
+    }
+}
---
+++ src/ImGui/Rendering/Pen.cs
@@ -0,0 +1,13 @@
+﻿using ImGui.Common.Primitive;
+
+namespace ImGui.Rendering
+{
+    /// <summary>
+    /// Describes how to stroke a shape.
+    /// </summary>
+    internal class Pen
+    {
+        public Color LineColor { get; set; } = Color.Black;
+        public float LineWidth { get; set; } = 1;
+    }
+}
