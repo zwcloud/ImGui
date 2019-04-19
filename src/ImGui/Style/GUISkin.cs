93d13f2708e51ab0d3826c51234dc2374fe59c55
Zou Wei <zouwei@hotmail.com>
2019-04-19 12:54:23 +0800

    fix

--- src/ImGui/Style/GUISkin.cs
+++ src/ImGui/Style/GUISkin.cs
@@ -41,10 +41,6 @@ namespace ImGui
             skin.InitCollapsingHeaderStyles(button, out var collapsingHeader);
             skin.styles[GUIControlName.CollapsingHeader] = collapsingHeader;
 
-            StyleRuleSet progressBar = new StyleRuleSet();
-            skin.InitProgressBarStyles(progressBar);
-            skin.styles[GUIControlName.ProgressBar] = progressBar;
-
             //skin.InitSelectableStyles();
             //skin.InitListBoxStyles();
             //skin.InitTextBoxStyles();
