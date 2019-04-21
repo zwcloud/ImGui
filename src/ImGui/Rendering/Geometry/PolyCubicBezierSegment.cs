dac41257f03c90b0aa30656a5b9e19d1f4f602fe
Zou Wei <zwcloud@hotmail.com>
2019-04-21 22:52:19 +0800

    added polygon segment types

---
+++ src/ImGui/Rendering/Geometry/PolyCubicBezierSegment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ImGui.Rendering
+{
+    /// <summary>
+    /// Represents one or more cubic Bezier curves.
+    /// </summary>
+    public class PolyCubicBezierSegment : PathSegment
+    {
+        /// <summary>
+        /// Gets or sets the list of Points that define this PolyCubicBezierSegment object.
+        /// </summary>
+        public List<Point> Points { get; set; }
+
+        public PolyCubicBezierSegment(IEnumerable<Point> points, bool isStroked)
+        {
+            if (points == null)
+            {
+                throw new System.ArgumentNullException(nameof(points));
+            }
+
+            Points = new List<Point>(points);
+            IsStroked = isStroked;
+        }
+    }
+}
---
+++ src/ImGui/Rendering/Geometry/PolyQuadraticBezierSegment.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ImGui.Rendering
+{
+    /// <summary>
+    /// Represents a set of quadratic Bezier segments.
+    /// </summary>
+    public class PolyQuadraticBezierSegment : PathSegment
+    {
+        /// <summary>
+        /// Gets or sets the list of Points that define this PolyQuadraticBezierSegment object.
+        /// </summary>
+        public List<Point> Points { get; set; }
+
+        public PolyQuadraticBezierSegment(IEnumerable<Point> points, bool isStroked)
+        {
+            if (points == null)
+            {
+                throw new System.ArgumentNullException(nameof(points));
+            }
+
+            Points = new List<Point>(points);
+            IsStroked = isStroked;
+        }
+    }
+}
