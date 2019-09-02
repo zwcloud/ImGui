015d2c685de26e63c7ce7de139f94266d969169e
Zou Wei <zwcloud@hotmail.com>
2019-09-02 19:22:26 +0800

    revert test code

--- src/ImGui/Rendering/DrawingContextEx.cs
+++ src/ImGui/Rendering/DrawingContextEx.cs
@@ -138,8 +138,7 @@ namespace ImGui.Rendering
 
         public static void DrawBoxModel(this DrawingContext dc, Node node)
         {
-            dc.DrawRectangle(new Brush(Color.LightBlue), new Pen(Color.Black, 2), node.Rect);
-            //DrawBoxModel(dc, node.RuleSet, node.Rect);
+            DrawBoxModel(dc, node.RuleSet, node.Rect);
         }
     }
 }
