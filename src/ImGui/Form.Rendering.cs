66cc3af0dcba957d2c5cdbfc5119245f39b2fade
Zou Wei <zwcloud@hotmail.com>
2020-03-16 22:12:03 +0800

    created background/foreground DrawingContext for rendering debug overlays

---
+++ src/ImGui/Form.Rendering.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ImGui.Rendering;
+
+namespace ImGui
+{
+    partial class Form
+    {
+        /// <summary>
+        /// First draw context to be rendered.
+        /// </summary>
+        public DrawingContext BackgroundDrawingContext { get; private set; }
+
+        /// <summary>
+        /// Last draw context to be rendered. This is where we the render most debug overlays.
+        /// </summary>
+        public DrawingContext ForegroundDrawingContext { get; private set; }
+
+        private Node backgroundNode = new Node(nameof(backgroundNode));
+
+        private Node foregroundNode = new Node(nameof(foregroundNode));
+
+        private readonly GraphicsImplementation.BuiltinGeometryRenderer backforegroudGeometryRenderer
+            = new GraphicsImplementation.BuiltinGeometryRenderer();
+
+        private MeshList backforegroudMeshList { get; set; } = new MeshList();
+
+        private RenderContext backforegroudRenderContext;
+
+        void InitializeBackForegroundRenderContext()
+        {
+            backforegroudRenderContext = new RenderContext(backforegroudGeometryRenderer, backforegroudMeshList);
+        }
+    }
+}
