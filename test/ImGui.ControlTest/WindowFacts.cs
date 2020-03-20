fca99a5cbc690e762bf7d4d4db767380ba30f305
Zou Wei <zwcloud@hotmail.com>
2020-03-20 22:11:37 +0800

    added a control test for foreground rendering

--- test/ImGui.ControlTest/WindowFacts.cs
+++ test/ImGui.ControlTest/WindowFacts.cs
@@ -20,5 +20,32 @@ namespace ImGui.ControlTest
 
             Application.Run(form);
         }
+
+        [Fact]
+        public void DrawOverlayOnForeground()
+        {
+            Rendering.PathGeometryBuilder g = new Rendering.PathGeometryBuilder();
+
+            g.BeginPath();
+            g.MoveTo(50, 140);
+            g.LineTo(150, 60);
+            g.LineTo(250, 140);
+            g.ClosePath();
+            g.Stroke();
+            var geometry = g.ToGeometry();
+
+            Rendering.Pen pen = new Rendering.Pen(Color.LightGreen, 2);
+
+            Application.IsRunningInUnitTest = true;
+            Application.Init();
+
+            var form = new MainForm(new Rect(320, 180, 800, 600));
+            form.OnGUIAction = () =>
+            {
+                form.ForegroundDrawingContext.DrawGeometry(null, pen, geometry);
+            };
+
+            Application.Run(form);
+        }
     }
 }
