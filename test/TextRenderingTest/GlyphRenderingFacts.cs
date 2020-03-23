bea38fd13ef217783bdd4afd1d329105dc2bbb95
Zou Wei <zwcloud@hotmail.com>
2020-03-23 23:17:40 +0800

    Extract DrawingVisual-to-image to utility class
    Method DrawDrawingVisualToImage is extracted to be reused later
    in other unit tests.

--- test/TextRenderingTest/GlyphRenderingFacts.cs
+++ test/TextRenderingTest/GlyphRenderingFacts.cs
@@ -140,48 +140,15 @@ namespace TextRenderingTest
             drawingContext.DrawGeometry(startPointBrush, null, startPointGeometry);
             drawingContext.Close();
 
-            //convert geometries inside the drawingVisual's content to meshes stored in a MeshList with a BuiltinGeometryRenderer
-            MeshList meshList = new MeshList();
-            BuiltinGeometryRenderer geometryRenderer = new BuiltinGeometryRenderer();
-            RenderContext renderContext= new RenderContext(geometryRenderer, meshList);
-            drawingVisual.RenderContent(renderContext);
-
-            //merge meshes in the MeshList to a MeshBuffer
-            MeshBuffer meshBuffer = new MeshBuffer();
-            meshBuffer.Clear();
-            meshBuffer.Init();
-            meshBuffer.Build(meshList);
-
-            //created a mesh IRenderer
-            var size = new Size(1000, 1000);
+            //draw the drawingVisual to image
             //TODO use a size of 2048x2048
             //BUG currently, using 2048x2048 will make the rendered image blank
-            Application.Init();
-            var window = Application.PlatformContext.CreateWindow(Point.Zero, size, WindowTypes.Regular);
-            var renderer = Application.PlatformContext.CreateRenderer();
-            renderer.Init(window.Pointer, window.ClientSize);
-            CSharpGL.GL.Viewport(0, 0, (int)size.Width, (int)size.Height);
-
-            //clear the canvas and draw mesh in the MeshBuffer with the mesh renderer
-            renderer.Clear(Color.White);
-            renderer.DrawMeshes((int)size.Width, (int)size.Height,
-                (
-                    shapeMesh: meshBuffer.ShapeMesh,
-                    imageMesh: meshBuffer.ImageMesh,
-                    textMesh: meshBuffer.TextMesh
-                )
-            );
-
-            //get drawn pixels data
-            var imageBytes = renderer.GetRawBackBuffer(out _, out _);
-
-            //clear native resources: window and IRenderer
-            renderer.ShutDown();
-            window.Close();
+            int width = 1000, height = 1000;
+            Util.DrawDrawingVisualToImage(out var imageBytes, width, height, drawingVisual);
 
             //save and show the image
             var path = $"{OutputDir}{Path.DirectorySeparatorChar}{nameof(ShowGlyphAsDirectedGraph_Builtin)}_{fontFileName}_{character.GetHashCode()}.png";
-            Util.ShowImageNotOpenFolder(imageBytes, (int)size.Width, (int)size.Height, path);
+            Util.ShowImageNotOpenFolder(imageBytes, width, height, path);
         }
 
         [Fact]
