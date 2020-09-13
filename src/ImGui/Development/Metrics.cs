6e5e15c55e7504254a4bf6c22e7173d7ac1173d8
Zou Wei <zwcloud@hotmail.com>
2020-09-13 21:49:04 +0800

    Arrange code

--- src/ImGui/Development/Metrics.cs
+++ src/ImGui/Development/Metrics.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
 using static ImGui.GUILayout;
 
@@ -15,15 +17,14 @@ namespace ImGui.Development
 
         public static void ShowWindow(ref bool windowOpened)
         {
-
-            if (!GUI.Begin(WindowName, ref windowOpened))
+            if(!GUI.Begin(WindowName, ref windowOpened))
             {
                 GUI.End();
                 return;
             }
             
             // Basic info
-            GUIContext g = Form.current.uiContext;
+            GUIContext g = GetCurrentContext();
             Text("Hello ImGui {0}##Metrics.Version", GetVersion());
             Text("Application average {0:F3} ms/frame ({1:F1} FPS)##Metrics.FPS", 1000.0f / g.fps, g.fps);
             Text("{0} vertices, {1} indices ({2} triangles)##Metrics.VI",
@@ -32,29 +33,8 @@ namespace ImGui.Development
             Separator("separator0");
 
             //Windows
-            if (TreeNode("Windows"))
-            {
-                var windowManager = GetCurrentContext().WindowManager;
-                var windows = windowManager.Windows;
-                for (int i = 0; i < windows.Count; i++)
-                {
-                    var window = windows[i];
-                    if (window == null)
-                    {
-                        BulletText("Window: null");
-                        continue;
-                    }
-
-                    if (TreeNode($"{(window.Collapsed ? "[ ]" : "[x]")}" +
-                                 $"Window[{i}] '{window.Name}', " +
-                                 $"{(window.Active || window.WasActive ? 1 : 0)}"))
-                    {
-                        Label($"(TODO: display window info here)#TODO_Window{i}");
-                    }
-                    TreePop();
-                }
-            }
-            TreePop();
+            var windowManager = g.WindowManager;
+            NodeWindows(windowManager.Windows, "Windows");
 
             GUI.End();
         }
@@ -66,6 +46,35 @@ namespace ImGui.Development
                 entryAssembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
                     .InformationalVersion;
             return informationalVersion;
+        }
+
+        private static void NodeWindows(IList<Window> windows, string label)
+        {
+            if(TreeNode("Windows"))
+            {
+                foreach (var window in windows)
+                {
+                    NodeWindow(window, "Window");
+                }
+            }
+            TreePop();
+        }
+
+        private static void NodeWindow(Window window, string label)
+        {
+            if(window == null)
+            {
+                BulletText("Window: null");
+                return;
+            }
+
+            if(TreeNode($"{(window.Collapsed ? "[ ]" : "[x]")}" +
+                         $"Window '{window.Name}', " +
+                         $"{(window.Active || window.WasActive ? 1 : 0)}"))
+            {
+                Label($"(TODO: display window info here)#TODO_Window{window.ID}");
+            }
+            TreePop();
         }
     }
 }
