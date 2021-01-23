925747322283226f86fc4da28d3d7930fb9324ec
Zou Wei <zwcloud@hotmail.com>
2021-01-23 21:46:31 +0800

    Enable multi-viewports by default

--- src/ImGui/GUIConfiguration.cs
+++ src/ImGui/GUIConfiguration.cs
@@ -3,7 +3,7 @@
     internal class GUIConfiguration
     {
         // See ImGuiConfigFlags_ enum. Set by user/application. Gamepad/keyboard navigation options, etc.
-        public ImGuiConfigFlags ConfigFlags;// = 0
+        public ImGuiConfigFlags ConfigFlags = ImGuiConfigFlags.ViewportsEnable;// = 0
         public ImGuiBackendFlags  BackendFlags;            // = 0              // See ImGuiBackendFlags_ enum. Set by backend (imgui_impl_xxx files or custom backend) to communicate features supported by the backend.
         public bool ConfigWindowsMoveFromTitleBarOnly { get; set; } = false;
     }
