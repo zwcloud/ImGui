02335b1bbff374c1b35174db72bab2043173a152
Zou Wei <zwcloud@hotmail.com>
2021-08-15 17:30:59 +0800

    Add a test project to solve multi-form issue
    We should reduce issue scale for easier analysis.

--- src/ImGui/Properties/AssemblyInfo.cs
+++ src/ImGui/Properties/AssemblyInfo.cs
@@ -3,4 +3,5 @@
 [assembly: InternalsVisibleTo("ImGui.UnitTest")]
 [assembly: InternalsVisibleTo("ImGui.ControlTest")]
 [assembly: InternalsVisibleTo("TextRenderingTest")]
+[assembly: InternalsVisibleTo("MultiFormTest")]
 [assembly: InternalsVisibleTo("TestUtil")]
