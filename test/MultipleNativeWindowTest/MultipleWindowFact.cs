340b2be34a96da6df2807e1fe325d825157fd7f9
Zou Wei <zwcloud@hotmail.com>
2021-08-23 13:10:42 +0800

    Show different mesh in parent and child window

--- test/MultipleNativeWindowTest/MultipleWindowFact.cs
+++ test/MultipleNativeWindowTest/MultipleWindowFact.cs
@@ -55,7 +55,8 @@ namespace MultipleNativeWindowTest
             windowB.Show();
 
             Win32OpenGLRenderer renderer = new Win32OpenGLRenderer(windowA);
-            var mesh = new QuadMesh(new Rect(100, 100, 200, 200), Color.Blue);
+            var meshA = new QuadMesh(new Rect(150, 150, 50, 50), Color.Blue);
+            var meshB = new QuadMesh(new Rect(10, 10, 50, 50), Color.DarkBlue);
 
             while (!windowA.Closed)
             {
@@ -63,12 +64,12 @@ namespace MultipleNativeWindowTest
                 {
                     renderer.SetRenderingWindow(windowA);
                     renderer.Clear(Color.Rgb(200, 100, 100));
-                    Win32OpenGLRenderer.DrawMesh(OpenGLMaterial.shapeMaterial, mesh, (int)windowA.ClientSize.Width, (int)windowA.ClientSize.Height);
+                    Win32OpenGLRenderer.DrawMesh(OpenGLMaterial.shapeMaterial, meshA, (int)windowA.ClientSize.Width, (int)windowA.ClientSize.Height);
                     renderer.SwapBuffers();
 
                     renderer.SetRenderingWindow(windowB);
                     renderer.Clear(Color.Rgb(100, 200, 100));
-                    Win32OpenGLRenderer.DrawMesh(OpenGLMaterial.shapeMaterial, mesh, (int)windowB.ClientSize.Width, (int)windowB.ClientSize.Height);
+                    Win32OpenGLRenderer.DrawMesh(OpenGLMaterial.shapeMaterial, meshB, (int)windowB.ClientSize.Width, (int)windowB.ClientSize.Height);
                     renderer.SwapBuffers();
 
                 });
