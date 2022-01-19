2d177563f96c2ef49f5dfb98b36cb637bfb5419d
Zou Wei <zwcloud@hotmail.com>
2022-01-20 00:36:23 +0800

    Show demo in AndroidTemplate

--- templates/AndroidTemplate/MainForm.cs
+++ templates/AndroidTemplate/MainForm.cs
@@ -5,10 +5,19 @@ namespace AndroidTemplate
     internal class MainForm : Form
     {
         public MainForm(Point position, Size size) : base(new Rect(position, size)) { }
+        
+        private static readonly Demo demo = new Demo();
+
+        static string[] text = {
+            "A", "BB", "CCC"
+        };
 
         public void OnGUI()
         {
-            GUILayout.Button("123");
+            demo.OnGUI();
+            GUI.Begin("Combobox demo", new Point(10, 10), new Size(400, 400));
+            GUI.ComboBox(new Rect(10, 10, 200, 20), text);
+            GUI.End();
         }
     }
 }
