72ffa8edd24d55cde4da20f465a29729435edbcb
Zou Wei <zwcloud@hotmail.com>
2022-03-24 06:19:08 +0800

    Add dev-only class GUIDebug for debugging GUI API related issues

---
+++ src/ImGui/Development/GUIDebug.cs
@@ -0,0 +1,14 @@
+﻿namespace ImGui.Development
+{
+    public class GUIDebug
+    {
+        public static void SetWindowPosition(string windowName, Point position)
+        {
+            var possibleWindow = Application.ImGuiContext.WindowManager.Windows.Find(window => window.Name == windowName);
+            if (possibleWindow != null)
+            {
+                possibleWindow.Position = position;
+            }
+        }
+    }
+}
