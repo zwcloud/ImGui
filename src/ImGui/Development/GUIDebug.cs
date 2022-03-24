6073c6ec0f756db7396d4e78bb2a611554832cf9
Zou Wei <zwcloud@hotmail.com>
2022-03-24 08:28:19 +0800

    Add DebugBox for debugging issues in popup windows

--- src/ImGui/Development/GUIDebug.cs
+++ src/ImGui/Development/GUIDebug.cs
@@ -1,4 +1,6 @@
-﻿namespace ImGui.Development
+﻿using ImGui.Rendering;
+
+namespace ImGui.Development
 {
     public class GUIDebug
     {
@@ -8,6 +10,44 @@
             if (possibleWindow != null)
             {
                 possibleWindow.Position = position;
+            }
+        }
+    }
+}
+
+namespace ImGui
+{
+    public partial class GUI
+    {
+        public static void DebugBox(int id, Rect rect, Color color)
+        {
+            var window = GetCurrentWindow();
+            if (window.SkipItems)
+                return;
+
+            //get or create the root node
+            var container = window.AbsoluteVisualList;
+            var node = (Node)container.Find(visual => visual.Id == id);
+            if (node == null)
+            {
+                //create node
+                node = new Node(id, $"DebugBox<{id}>");
+                node.UseBoxModel = true;
+                node.RuleSet.Replace(GUISkin.Current[GUIControlName.Box]);
+                node.RuleSet.BackgroundColor = color;
+                container.Add(node);
+            }
+            node.ActiveSelf = true;
+            
+            // last item state
+            window.TempData.LastItemState = node.State;
+
+            // rect
+            node.Rect = window.GetRect(rect);
+
+            using (var dc = node.RenderOpen())
+            {
+                dc.DrawBoxModel(node.RuleSet, node.Rect);
             }
         }
     }
